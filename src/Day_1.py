247bc24f6c015b5f5b4d47b8c385f4ae5edb9748
TheAmazingHazza <harry@robvallance.com>
2021-12-02 23:20:35 +0000

    Reformatted Day_1.py

--- src/Day_1.py
+++ src/Day_1.py
@@ -15,31 +15,15 @@ def get_times_increased(nums):
     return times_increased
 
 
-def divide_chunks(l, n):
-    # looping till length l
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
+# Part 2
 def get_times_increased_window(nums):
-    three_measurements = list(divide_chunks(nums, 3))
-    three_measurements = [s for s in three_measurements if not len(s) < 3]
-
-    totals = []
-    for measurement in three_measurements:
-        totals.append(sum(measurement))
-    # print(totals)
-
     three_measurement_windows = []
 
     for lower_bound in range(len(nums) - 2):
         upper_bound = lower_bound + 3
-        print(nums[lower_bound:upper_bound])
         three_measurement_windows.append(sum(nums[lower_bound: upper_bound]))
-    # print(three_measurement_windows)
     return get_times_increased(three_measurement_windows)
 
-    # return get_times_increased(totals)
 
-
-# print(get_times_increased(values))
+print(get_times_increased(values))
 print(get_times_increased_window(values))
